0d19b2f20c0b20af9ba5951582c5c32715fb15f8
Mohamed Aslam <mohamedaslamcodes@gmail.com>
2024-08-05 09:37:12 +0530

    width fix

--- EduQuestUI/src/components/Course/CourseLanding.tsx
+++ EduQuestUI/src/components/Course/CourseLanding.tsx
@@ -73,7 +73,7 @@ const CourseLanding = () => {
           {course.courseThumbnailPicture && (
             <img
               src={course.courseThumbnailPicture as string}
-              className=" rounded-lg shadow-2xl max-w-full md:w-md"
+              className=" rounded-lg shadow-2xl w-56"
             />
           )}
           <div className="md:max-w-lg">
